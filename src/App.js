a9bdfed12a524d7053f87f3d30a51f6d30b507d9
Jelena Feliciano <jfel217@gmail.com>
2021-07-26 23:02:52 -0400

    initial commit install react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './css/index.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The Gallery App
         </p>
         <a
           className="App-link"
