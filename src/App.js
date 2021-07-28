bf4da269cf9c916373c4423662149566dbfcb536
Jelena Feliciano <jfel217@gmail.com>
2021-07-27 22:40:35 -0400

    added API info

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import './index.css';
-//import axios from 'axios';
-//import SearchForm from './Components/SearchForm';
+import './css/index.css';
+import axios from 'axios';
+import SearchForm from './Components/SearchForm';
 import PicList from './Components/PicList';
-
+import apiKey from './config';
 
 
 export default class App extends Component {
@@ -14,14 +14,20 @@ export default class App extends Component {
       loading: true
     };
   }
-  // componentDidMount(){
-  //   this.performSearch()
-  // }
+  componentDidMount(){
+    this.performSearch()
+  }
 
   //pass the query the value 'dogs' to prevent an empty page onload
-  // performSearch = (query = 'dogs') => {
-  //   axios.get('https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=a10abb8336a90173442222b094e5852b&tags=cat%2C+dog%2C+bird&per_page=25&format=json&nojsoncallback=1');
-  // }
+  performSearch = (query = 'dogs') => {
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=25&format=json&nojsoncallback=1`)
+    .then(res => {
+      this.setState({
+        pics: res.data,
+        loading: false
+      });
+    })
+  }
   render() {
     console.log(this.state.pics) 
     return(
@@ -29,6 +35,8 @@ export default class App extends Component {
         <div className="inner">
           <h2>The Gallery App</h2>
           {/** Enter Search bar */}
+          <SearchForm />
+          
         </div>
         <div className="main-content">
         {
--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
-// import{ Form, 
-//     FormGroup,
-//     FormControl,
-//     Button,
-    
-//  } from 'react-bootstrap';
-// import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { FaSearch } from 'react-icons/fa';
 
 export default class SearchForm extends Component {
     state = {
@@ -13,7 +8,7 @@ export default class SearchForm extends Component {
     }
 
     onSearchChange = e => {
-        this.setState({ searchText: e.target. value });
+        this.setState({ searchText: e.target.value });
     }
 
     handleSubmit = e => {
@@ -21,14 +16,20 @@ export default class SearchForm extends Component {
     
         //invoke the performSearch function that fetches data
         this.props.onSearch(this.query.value);
-        e. currentTarget.reset();
+        e.currentTarget.reset();
     }
 
     render(){
         return(
             <div className="container">
-                <form className="search-form">
-
+                <form className="search-form" onSubmit={this.handleSubmit}>
+                    <input type="search"
+                        onChange={this.onSearchChange}
+                        name="search"
+                        ref={(input) => this.query=input}
+                        placeholder="Search..." 
+                    />
+                <button type='submit' id="submit" className="search-form-button"><FaSearch /></button>
                 </form>
             </div>
             
