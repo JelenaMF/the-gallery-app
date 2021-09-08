873264a68a59e46cc4bd0bacd51641de356fdb61
Jelena Feliciano <jfel217@gmail.com>
2021-09-08 14:24:53 -0400

    fixed the search route that was adding search everytime a search was made

--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -16,9 +16,9 @@ import { withRouter } from 'react-router';
     handleSubmit = e => {
         e.preventDefault();
         //set path with searchText
-        let path = `search/${this.state.searchText}`
+        let path = `${this.state.searchText}`
         this.props.onSearch(this.query.value);
-        this.props.history.push(path);
+        this.props.history.push(`/search/${path}`);
         e.currentTarget.reset();
     }
 
