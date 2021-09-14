db3482bfc64468c014652905dac003a9f58bbf80
Jelena Feliciano <jfel217@gmail.com>
2021-09-14 17:28:21 -0400

    fixed the issue with the links I was having Now to fix the search input again

--- src/App.js
+++ src/App.js
@@ -2,93 +2,16 @@
 import React, { Component} from "react";
 import Switch from "react-bootstrap/esm/Switch";
 import { BrowserRouter, Route } from "react-router-dom";
-import axios from 'axios';
 import './css/index.css';
 //importing components
 import SearchForm from './Components/SearchForm';
 import PhotoList from './Components/PhotoList';
-import apiKey from './config';
+
 import Nav from './Components/Nav'
 import NotFound from './Components/NotFound'
 
 
 export default class App extends Component {
- 
-    constructor(props) {
-      super(props);
-      /** 
-        * set initial state of photos to an empty array 
-        * set the initial state of searchText to an empty string
-        * Set the initial state of loading to true
-        * set the the initial state of title to empty string.
-       * 
-       */
-      
-      this.state = { 
-        photos:[],
-        searchText:'',  
-        loading: true,
-        title: ''
-      };
-    }
-
-     /**
-     * mounts the api from the `getPhoto`
-     * @param dogs - initial query 
-     */
-
-      componentDidMount(){
-        this.getPhoto('dogs')
-      }
-
-    /** 
-     * `getPhoto` requests an api and sets the state properties
-     * @param {string} query - a name of query
-     * @param {Array} photos - an array of photo url from the API 
-     * @param {bool} loading - sets to false. 
-     * @param {string} title - sets title of the searched query to uppercase letters. 
-     * 
-     * catches fetching/parsing errors.  
-     * */
-    
-    getPhoto = (query = this.searchText) => { 
-      const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`;
-      axios.get(url)
-      .then(res => {
-        const data = res.data.photos.photo;
-        const picArray = data.map((photoData) => {
-          const id= photoData.id;
-          const server= photoData.server;
-          const secret = photoData.secret;
-          return `https://live.staticflickr.com/${server}/${id}_${secret}.jpg`;
-        });
-        this.setState({
-          photos: picArray, 
-          loading: false,
-          title: query.toUpperCase()
-        });
-        
-      })
-      .catch( err => {
-        console.log('Error fetching and parsing data', err)
-      })
-    }
-
-   
-
-     /**`componentDidUpdate - compares the previous state of the searchText/title to the current state of the searchText/title
-      * if they don't match the `getPhoto` method calls the current state of the searchText. 
-       */
-    componentDidUpdate(prevProps, prevState) { 
-      console.log('componentdidupdate');
-      if(prevState.searchText !== this.state.searchText){
-        this.getPhoto(this.state.searchText)
-        }
-    }
-
-    componentWillUnmount(prevProps){
-      this.getPhoto(prevProps)
-    }
     
     render() {
       return(
@@ -99,24 +22,22 @@ export default class App extends Component {
             </div>
             <div className="container">
             <BrowserRouter>
-              <SearchForm onSearch={this.getPhoto}  />   
-              <Nav /> 
-              <div className="photo-container">
-                <h1>{this.state.title}</h1>
+            <SearchForm  />   
+             <Nav /> 
                 {
-                  (this.state.loading)
-                  ?<h2>loading...</h2>
-                  : <Switch>
-                    <Route  path="/" render={() =>  <PhotoList data={this.state.photos} /> } />
-                    <Route exact path="/:query" render={() => <PhotoList data={this.state.searchText} />}  /> 
-                    <Route path="/birds" render={() => {this.getPhoto('birds')}} />  
-                    <Route path="/frogs" render={() => {this.getPhoto('frogs')}} />
-                    <Route path="/cats" render={() => {this.getPhoto('cats')}} />
-                    <Route path="/cats" render={() => <NotFound />} />
-
+                  <Switch>
+{/* {                    <Route exact path="/:query" render={() => <PhotoList  />}  />  */}
+                   <Route path="/birds"component={PhotoList} />  
+                    <Route path="/frogs" component={PhotoList} />
+                    <Route path="/cats" component={PhotoList} />
+                    {/* add a conditional statement here or in the SearchFrom file or in the PhotoList 
+                      that will create a 'notfound route that displays the NotFound component page. 
+                    */}
+                    <Route path="/notfound" render={() => <NotFound />} />
+                    <Route exact path="/" component={ PhotoList } />
                   </Switch>
                 }
-                </div>
+              
             </BrowserRouter>     
             </div>
           </div>
--- src/Components/PhotoList.js
+++ src/Components/PhotoList.js
@@ -1,30 +1,86 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Photo from './Photo';
 import NotFound from './NotFound';
+import axios from 'axios';
+import  {match, withRouter} from 'react-router'
+import apiKey from '../config';
+export default class PhotoList extends Component {
+    constructor(props) {
+        super(props);
+        /** 
+          * set initial state of photos to an empty array 
+          * set the initial state of searchText to an empty string
+          * Set the initial state of loading to true
+          * set the the initial state of title to empty string.
+         * 
+         */
+        
+        this.state = { 
+          photos:[],
+          searchText:'',  
+          loading: true,
+          title: ''
+        };
+      }
 
-const PhotoList = props => {
-    let results = props.data;
-    const alt = props.alt;
-    let list = [];
-    if(results.length > 0){
-         
-        for(let i = 0; i < results.length; i++){
-            list.push(<Photo url={results[i]} alt={alt} key={i}  />)
-        }
-        
+      componentDidMount(){
+          const searchText = this.props.location.pathname.replace('/', '');
+          if(searchText === ''){
+              this.getPhoto('dogs');
+          } else {
+            this.getPhoto(searchText);
+
+          }
+      }
+
+        /** 
+     * `getPhoto` requests an api and sets the state properties
+     * @param {string} query - a name of query
+     * @param {Array} photos - an array of photo url from the API 
+     * @param {bool} loading - sets to false. 
+     * @param {string} title - sets title of the searched query to uppercase letters. 
+     * 
+     * catches fetching/parsing errors.  
+     * */
     
-    } else {
-        /** route to no-results-layout */
-        <NotFound />
-    } 
-    return(
-        <div className="photo-container">
-            <h2> {props.title} </h2>
-            <ul>
-                {list}
-            </ul>
-        </div>
-        
-    )
+         getPhoto = (query) => { 
+            
+            const url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`;
+            axios.get(url)
+            .then(res => {
+              const data = res.data.photos.photo;
+              const picArray = data.map((photoData) => {
+                const id= photoData.id;
+                const server= photoData.server;
+                const secret = photoData.secret;
+                return `https://live.staticflickr.com/${server}/${id}_${secret}.jpg`;
+              });
+              this.setState({
+                photos: picArray, 
+                loading: false,
+                title: query.toUpperCase()
+              });
+              
+            })
+            .catch( err => {
+              console.log('Error fetching and parsing data', err)
+            })
+          }
+    render(){
+        console.log(this.props);   
+        const { photos, title } = this.state;
+        const photoList = photos.map((photo) => {
+           return <Photo url={photo} alt={title} key={photo} />
+        })
+        return(
+            <div className="photo-container">
+                <h2> {title} </h2>
+                <ul>
+                    {photoList}
+                </ul>
+            </div>
+            
+        )
+    }
+    
 }
-export default PhotoList;
