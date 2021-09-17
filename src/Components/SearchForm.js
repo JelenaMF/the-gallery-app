c73afc566c23ab87545d139c13f526c09d6aa48d
Jelena Feliciano <jfel217@gmail.com>
2021-09-17 16:31:06 -0400

    reverted changes made to the SearchForm

--- src/Components/SearchForm.js
+++ src/Components/SearchForm.js
@@ -2,44 +2,42 @@ import React, { Component } from 'react';
 import { FaSearch } from 'react-icons/fa';
 import { withRouter } from 'react-router';
 
-
 //importing components
 import NotFound from '../Components/NotFound';
 import PhotoList from '../Components/PhotoList';
 
  class SearchForm extends Component {
+    state = {
+        query: '',
+        data: [],
+        searchText: ''
 
+    }
 
-        state = {
-            query: '',
-            loading: true
-        }
- 
-    
-    
     onSearchChange = e => {
         this.setState({ 
             query: e.target.value, 
+            searchText: this.query.value
         });
+            
     }
 
-   
     handleSubmit = e => {
         e.preventDefault();
         //set path with searchText
-        let path = `${this.state.query}`;
+        let path = `${this.state.query}`
         this.props.history.push(`/search/${path}`);
-        this.setState({query: '', loading:false})
+        this.setState({query: ''})
     }
+    
 
     render(){
-        const {query} = this.state;
         return(
-                <form className="search-form" onSubmit={this.handleSubmit} >
+                <form className="search-form" onSubmit={this.handleSubmit}>
                     <input id="search" type="search"
                         onChange={this.onSearchChange}
                         name="search"
-                        value={query}
+                        value={this.state.query}
                         ref={(input) => this.query=input}
                         placeholder="Search..." 
                     />
